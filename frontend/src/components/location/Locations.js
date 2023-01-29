3932a16f5086cfbf1b3c2d2218090855777aef95
Colin Le <colintranle123@gmail.com>
2023-01-28 19:14:35 -0500

    Revised folders Thumbnail

--- frontend/src/components/location/Locations.js
+++ frontend/src/components/location/Locations.js
@@ -1,13 +1,40 @@
 import React from 'react'
-import LocationList from './LocationList';
 import Map from '../map/Map';
+import Thumbnail from '../thumbnail/Thumbnail';
+
+import { useSelector } from 'react-redux';
+import { useGetLocationsMutation } from '../../reducers/locations/locationSlice';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Locations() {
+
+  const coordinates = useSelector((state) => state.coordinates);
+  const [locations, setLocations] = useState([]);
+  const [getLocations, {isLoading}] = useGetLocationsMutation();
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const fetchLocations = async () => {
+      const locations = await getLocations(coordinates).unwrap();
+      console.log(locations);
+    }
+    if (coordinates.lat != null && coordinates.long != null)
+    {
+      fetchLocations();
+    }
+    else{
+      navigate("/");
+    }
+  }, []);
+
+
   return (
     <>
       <h1>Locations</h1>
       <Map/>
-      <LocationList/>
+      <Thumbnail/>
     </>
   )
 }
